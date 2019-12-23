fb686bfc7decb62276178d0d170809d46f4604e2
atrevalis <atrevalis@mail.ru>
2019-12-23 21:50:11 +0300

    Ввод из файла

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-int dimA;
-int dimB;//хуйня не работает
+int dimA;//по возможности заменить функцией
+int dimB;//по возможности заменить функцией
 using namespace std;
-int** ConsoleInsert(bool check,int **&array, char symbol);
-int** FileInsert(bool check,int **&array, char symbol);
+int** consoleInsert(bool check,int **&array, char symbol);
+int** fileInsert(bool check,int **&array, char symbol);
 void editArray(bool check,int **&array,int dim);
-
-void outputArray(bool check, int **&Array, int dim);
+void fileOutput(bool check, int **&array, int dim);
+void consoleOutput(bool check, int **&array, int dim);
+int** calculate(int **&arrayA,int **&arrayB, bool checkA,bool checkB);
 
 int main() {
-    char A = 'A';
-    char B = 'B';
+    char A = 'A';//symbol
+    char B = 'B';//symbol
     char f;//номер функции
     bool exit = false;// выход Д/Н
     int ins;//номер матрицы
@@ -20,7 +21,7 @@ int main() {
     int **arrayB;
     bool checkA = false;//чекер заполнения массива А
     bool checkB = false;//чекер заполнения массива Б
-    while(exit == false){
+    while(!exit){
         cout<<"Выберите дальнейшее действие:"<<"\n";
         cout<<"1:Ввод матрицы "<<"\n";
         cout<<"2:Вывод матрицы "<<"\n";
@@ -39,10 +40,10 @@ int main() {
                         cout<<"Чтобы вернутся в меню введите любое значение кроме предложенных"<<"\n";
                         cin>>(ins);//ПОПРАВИТЬ ОШИБКУ С ВВОДОМ НЕ ЧИСЕЛ
                         switch(ins){
-                            case 1: arrayA = ConsoleInsert(checkA,arrayA,A);
+                            case 1: arrayA = consoleInsert(checkA,arrayA,A);
                             checkA = true;
                             break;
-                          case 2: arrayA = FileInsert(checkA,arrayA,A);
+                          case 2: arrayA = fileInsert(checkA,arrayA,A);
                           checkA = true;
                           break;
                             default:break;
@@ -53,15 +54,16 @@ int main() {
                         cout<<"Чтобы вернутся в меню введите любое значение кроме предложенных"<<"\n";
                         cin>>(ins);//ПОПРАВИТЬ ОШИБКУ С ВВОДОМ НЕ ЧИСЕЛ
                         switch(ins){
-                            case 1: arrayB = ConsoleInsert(checkB,arrayB,B);
+                            case 1: arrayB = consoleInsert(checkB,arrayB,B);
                                 checkB = true;
                                 break;
-                            case 2: arrayB = FileInsert(checkB,arrayB,B);
+                            case 2: arrayB = fileInsert(checkB,arrayB,B);
                                 checkB = true;
                                 break;
                             default:break;
                         }
                         break;
+                    default:break;//заглушка
                 }
                 break;
             case '2':
@@ -69,17 +71,28 @@ int main() {
                 cin>>(ins);
                 switch(ins){
                     case 1:
-                        outputArray(checkA,arrayA,dimA);
+                        consoleOutput(checkA,arrayA,dimA);
                         break;
-                    case '2':
-                        outputArray(checkB,arrayB,dimB);
+                    case 2:
+                        consoleOutput(checkB,arrayB,dimB);
                         break;
                     default:
                         break;
                 }
                 break;
             case '3':
-                break;
+                cout<<"Выберите, какую матрицу вы хотите отредактировать"<<"\n"<<"1:А"<<"\n"<<"2:Б"<<"\n";
+                cin>>(ins);
+                switch(ins) {
+                    case 1:
+                        editArray(checkA, arrayA, dimA);
+                        break;
+                    case 2:
+                        editArray(checkB, arrayB, dimB);
+                        break;
+                    default:
+                        break;
+                }
             case '4': break;
             case '5':
                 exit = true;
@@ -92,8 +105,8 @@ int main() {
     }
 }
 
-void outputArray(bool check, int **&array, int dim) {
-    if (check == true) {
+void consoleOutput(bool check, int **&array, int dim) {//DONE
+    if (check) {
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
                 cout << array[i][j]<<" ";
@@ -103,7 +116,9 @@ void outputArray(bool check, int **&array, int dim) {
     }else{cout<<"Массива не существует"<<"\n";}
 }
 
-int** ConsoleInsert(bool check,int **&array,char symbol){//ПРОВЕРИТЬ ОЧИСТКУ ПАМЯТИ И ЕЕ ЗАПОЛНЕНИЕ
+void fileOutput(bool check, int **&array, int dim){}
+
+int** consoleInsert(bool check,int **&array,char symbol){//DONE
     if(check){delete[] array;}
     int dim;
     cout<<"Введите размерность квадратной матрицы"<<"\n";
@@ -120,20 +135,25 @@ int** ConsoleInsert(bool check,int **&array,char symbol){//ПРОВЕРИТЬ О
     }
     return array;
 }
-int** FileInsert(bool check,int **&array,char symbol){//Может быть проблема с Переводом чар в инт
+int** fileInsert(bool check,int **&array,char symbol){//NOTDONE(check)
     if(check){delete[] array;}
+    int length = 0;
+    int dim = 0;
+    int temp=0;
     bool  exit=false;
     string path;
-    cout<<"Введите путь файла"<<"\n";
-    cin>>path;
     ifstream matrix;
-    matrix.open(path);
-    while(exit==false) {
+    while(!exit) {
+        cout<<"Введите путь файла"<<"\n";
+        cin>>path;
+        matrix.open(path);
         if (matrix.is_open()) {
             matrix.seekg(0, ios_base::end);
-            int dim = (int) sqrt((double) matrix.tellg());//нахождение размерности
-            if(symbol == 1){ dimA = dim;}
-            if(symbol == 2){ dimB = dim;}
+            int a = matrix.tellg();
+            int dim = (int) sqrt((double) matrix.tellg()/2);//нахождение размерности
+            matrix.seekg(0, ios_base::beg);
+            if(symbol == 'A'){ dimA = dim;}
+            if(symbol == 'B'){ dimB = dim;}
             array = new int*[dim];//создание строк
             for(int i = 0;i<dim;i++){array[i]=new int[dim];}//создание столбцов
             for(int i=0;i<dim;i++){
@@ -147,27 +167,26 @@ int** FileInsert(bool check,int **&array,char symbol){//Может быть пр
     matrix.close();
     return array;
 }
-void editArray(bool check,int **&array,int dim){
+void editArray(bool check,int **&array,int dim){//DONE
     int column;
     int line;
     char trigger;
     bool exit = false;
-    if(check== true) {
-        while (exit == false) {
-            cout << "Выберите элемент, который вы хотите изменить" << "\n";;
-            cout << "Введите строку в которой содержится элемент" << "\n";;
+    if(check) {
+        while (!exit) {
+            cout <<"Выберите элемент, который вы хотите изменить" << "\n";
+            cout <<"+===========================================+"<< "\n";
+            cout << "Введите строку в которой содержится элемент" << "\n";
             cin >> line;
-            cout << "Введите столбец в котором содержится элемент" << "\n";;
+            cout << "Введите столбец в котором содержится элемент" << "\n";
             cin >> column;
-            cout << "Введите новое значение " << "\n";;
-            cin >> array[line][column];
-            cout << "Продолжить изменение массива? Y/N";
+            cout << "Введите новое значение " << "\n";
+            cin >> array[--line][--column];
+            cout << "Продолжить изменение массива? Y/N"<< "\n";
             cin >> trigger;
             if ((trigger == 'N') or (trigger == 'n')) { exit = true; }
         }
-    } else{cout<<"Массива не существует";}
+    } else{cout<<"Массива не существует"<<"\n";}
 }
-void Calculate(int **&arrayA,int **&arrayB, int dimA, int dimB, bool checktrue){
-
-}
+int** calculate(int **&arrayA,int **&arrayB, bool checkA,bool checkB){}
 
